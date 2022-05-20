4d3c5b695b55c7ad5f08b7044dceae7f99fad98d
miwytt <79340976+miwytt@users.noreply.github.com>
2022-05-20 11:52:45 +0200

    changed vocab_size to 100 for dry run; changed paths; removed typos

--- preprocess.sh
+++ preprocess.sh
@@ -1,14 +1,19 @@
 #! /bin/bash
 
-VOCAB_SIZE=50
-DATA_DIR=./data/os18_preprocessed/projects/tir1/corpora/dialogue_mt/aligned-os18-enfr
+VOCAB_SIZE=100
+#DATA_DIR=./data/os18_preprocessed/projects/tir1/corpora/
+#dialogue_mt/aligned-os18-enfr
+
+DATA_DIR=/Users/michelle/Desktop/cl/reproducibility_in_nlp/project/try/contextual-mt/data/os18_preprocessed/projects/tir1/corpora/dialogue_mt
+
+mkdir /Users/michelle/Desktop/cl/reproducibility_in_nlp/project/try/contextual-mt/data/os18_preprocessed/projects/tir1/corpora/dialogue_mt/prep
 
 for lang in en fr; do
     python ./scripts/spm_train.py \
         $DATA_DIR/train.${lang} \
         --model-prefix $DATA_DIR/prep/spm.${lang} \
-        #--vocab-file $DATA_DIR/prep/dict.${lang}.txt \
-        #--vocab-size $VOCAB_SIZE
+        --vocab-file $DATA_DIR/prep/dict.${lang}.txt \
+        --vocab-size $VOCAB_SIZE
 done
 for split in train valid test; do
     for lang in en fr; do
@@ -18,11 +23,12 @@ for split in train valid test; do
                 > $DATA_DIR/prep/${split}.sp.${lang}
     done
 done
+
 fairseq-preprocess \
-    --source-lang src --target-lang tgt \
+    --source-lang en --target-lang fr \
     --trainpref $DATA_DIR/prep/train.sp \
-    --validpref $DATA_DIR/pep/valid.sp \
+    --validpref $DATA_DIR/prep/valid.sp \
     --testpref $DATA_DIR/prep/test.sp \
-    --srcdict $DATA_DIR/prep/dict.src.txt \
-    --tgtdict $DATA_DIR/prep/dict.tgt.txt \
+    --srcdict $DATA_DIR/prep/dict.en.txt \
+    --tgtdict $DATA_DIR/prep/dict.fr.txt \
     --destdir $DATA_DIR/bin
