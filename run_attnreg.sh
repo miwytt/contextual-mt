69f368c91a9c7775ef8505cf204ec3eb98e95ece
miwytt <79340976+miwytt@users.noreply.github.com>
2022-06-03 21:47:25 +0200

    adjusted parameters according to paper

--- run_attnreg.sh
+++ run_attnreg.sh
@@ -12,20 +12,19 @@ TGT_CONTEXT_SIZE=5
 fairseq-train $PATH_TO_DATA \
     --user-dir $REPO \
     --task $TASK \
-    --source-lang en \
-    --target-lang fr \
     --source-context-size $SRC_CONTEXT_SIZE --target-context-size $TGT_CONTEXT_SIZE \
-    --sample-context-size \
-    --coword-dropout 0.1 \
+    --source-lang en --target-lang fr \
     --log-interval 10 \
-    --arch contextual_transformer --share-decoder-input-output-embed  \
+    --arch attn_reg_transformer --share-decoder-input-output-embed  \
     --optimizer adam --adam-betas '(0.9, 0.98)' --clip-norm 0.1 \
     --lr 5e-4 --lr-scheduler inverse_sqrt  --warmup-updates 4000 \
-    --criterion label_smoothed_cross_entropy --label-smoothing 0.1 --dropout 0.3 --weight-decay 0.0001 \
-    --max-tokens  4096 --update-freq 8 --patience 10 --seed 42 \
+    --criterion attention_loss --label-smoothing 0.1 --dropout 0.3 --weight-decay 0.0001 \
+    --regularize-heads 0 --regularize-attention enc cross self \
+    --enc-alignment-layer 0 --cross-alignment-layer 0 --self-alignment-layer 5 \
+    --highlight-sample 0.2 --kl-lambda 10 \
+    --max-tokens  4096 --max-tokens-valid 1024 --update-freq 8 --seed 42 \
     --eval-bleu \
     --eval-bleu-args '{"beam": 5, "max_len_a": 1.2, "max_len_b": 10}' \
     --eval-bleu-remove-bpe sentencepiece \
-    --eval-bleu-print-samples \
     --best-checkpoint-metric bleu --maximize-best-checkpoint-metric \
-    --save-dir ./checkpoints_attnreg --no-epoch-checkpoints 
+    --save-dir ./checkpoints_attnreg --no-epoch-checkpoints \
