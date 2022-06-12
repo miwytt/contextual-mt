4e256516337dee1e3acba2878170b8c0a4693e89
michelle <michelle.wastl@gmail.com>
2022-06-12 06:17:18 +0000

    runs w/ gold data

--- run_inference.sh
+++ run_inference.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-DATA_DIR=./data/os18/bin
+DATA_DIR=./data/os18/bin_actual
 #if bin does not work use the following:
 #DATA_DIR=./data/os18/prep
 
@@ -13,20 +13,20 @@ CHECKPOINT_DIR=./checkpoints_baseline
 # mkdir predictions_baseline_gold
 #PREDICTION_DIR=./predictions_baseline_gold
 mkdir predictions_baseline_gold
-PREDICTION_DIR=./predictions_baseline_nongold
+PREDICTION_DIR=./predictions_baseline_gold
 #mkdir predictions_attnreg_gold
 #PREDICTION_DIR=./predicitons_attnreg_gold
 #mkdir predictions_attnreg_nongold
 #PREDICTION_DIR=./predictions_attnreg_nongold
 
-cp $DATA_DIR/dict.*txt $CHECKPOINT_DIR
+cp $DATA_DIR/dict.*txt $DATA_DIR/spm* $CHECKPOINT_DIR
 
 python ./contextual_mt/docmt_translate.py \
     --path $CHECKPOINT_DIR \
     --source-lang en --target-lang fr \
     --source-file $DATA_DIR/test.en \
-    --predictions-file $PREDICTION_DIR/test.pred.en \
+    --predictions-file $PREDICTION_DIR/test.pred.fr \
+    --reference-file $DATA_DIR/test.fr \
     --docids-file $DATA_DIR/test.ids \
-    --beam 5
-#do gold if time    
-#--gold-target-context
+    --beam 5 \
+    --gold-target-context
